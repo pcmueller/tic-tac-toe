fe4ea1262819f6d19652e8bdff457506231d33a0
pcmueller <pmuellerleile@gmail.com>
2021-03-04 22:24:38 -0700

    Add Game class constructor

---
+++ game.js
@@ -0,0 +1,10 @@
+class Game {
+  constructor() {
+    this.player1 = new Player('one', ❤️);
+    this.player2 = new Player('two', ⭐️);
+    this.board = [];
+    this.currentPlayer = 1;
+    this.turnCounter = 0;
+    this.isActive = true;
+  }
+}
