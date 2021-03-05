96512a241ee4e017dbcaa4c462d164bb3d37ac5f
pcmueller <pmuellerleile@gmail.com>
2021-03-04 22:26:15 -0700

    Add Game checkForWinner method

--- game.js
+++ game.js
@@ -7,4 +7,26 @@ class Game {
     this.turnCounter = 0;
     this.isActive = true;
   }
+
+  checkForWinner() {
+    if (this.board[0] === this.board[1] === this.board[2]) {
+      this.declareVictory();
+    } else if (this.board[3] === this.board[4] === this.board[5]) {
+      this.declareVictory();
+    } else if (this.board[6] === this.board[7] === this.board[8]) {
+      this.declareVictory();
+    } else if (this.board[0] === this.board[3] === this.board[6]) {
+      this.declareVictory();
+    } else if (this.board[1] === this.board[4] === this.board[7]) {
+      this.declareVictory();
+    } else if (this.board[2] === this.board[5] === this.board[8]) {
+      this.declareVictory();
+    } else if (this.board[0] === this.board[4] === this.board[8]) {
+      this.declareVictory();
+    } else if (this.board[2] === this.board[4] === this.board[6]) {
+      this.declareVictory();
+    } else {
+      this.checkForDraw();
+    }
+  }
 }
