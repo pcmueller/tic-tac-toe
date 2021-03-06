882d71fe347680af214341460fae12b5ad054226
pcmueller <pmuellerleile@gmail.com>
2021-03-05 21:48:26 -0700

    Add selector variables to main.js

---
+++ src/main.js
@@ -0,0 +1,8 @@
+// selector variables
+
+var board = document.getElementById("boardContainer");
+var gameText = document.getElementById("gameText");
+var gameGrid = document.getElementById("gameGrid");
+var box = document.querySelectorAll("#box");
+var winCounter1 = document.getElementById("winCounter1");
+var winCounter2 = document.getElementById("winCounter2");
